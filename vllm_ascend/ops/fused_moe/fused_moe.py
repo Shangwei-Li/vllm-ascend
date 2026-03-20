0a2f30665953b46e200d0271d65b8e768c541311
Shangwei-Li <lishangwei@mail.ustc.edu.cn>
2026-03-20 12:00:38 +0800

    Move transpose to patch_loader.

--- vllm_ascend/ops/fused_moe/fused_moe.py
+++ vllm_ascend/ops/fused_moe/fused_moe.py
@@ -80,6 +80,8 @@ class AscendUnquantizedFusedMoEMethod(UnquantizedFusedMoEMethod):
         w2_data = self._maybe_pad_weight(layer.w2_weight.data).transpose(
             1, 2).contiguous()
         layer.w2_weight = torch.nn.Parameter(w2_data, requires_grad=False)
+        setattr(layer.w13_weight, "transposed", True)
+        setattr(layer.w2_weight, "transposed", True)
 
         if get_ascend_device_type() != AscendDeviceType._310P:
             layer.w13_weight.data = maybe_trans_nz(layer.w13_weight.data)
